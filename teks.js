3227ad7ed335d44b5f9996a71938ae94cb900f7a
FIKRI6969 <105841100821@student.unismuh.ac.id>
2024-05-20 14:43:09 +0800

    oke gass

--- teks.js
+++ teks.js
@@ -1,8 +1,6 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image, Button } from "react-native";
 
-
-export default function App() {
+const teks = () => {
   return (
     <View
       style={{
@@ -61,12 +59,3 @@ export default function App() {
     </View>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
